e2453a494ab3c3e9ff11953329d0ecb8875c3ea4
Techzjc <techzjc@vt.edu>
2022-09-30 13:04:33 -0400

    Feat: git binary checker before installation
    Add the feature for checking git before installing to avoid errors.

--- install.sh
+++ install.sh
@@ -8,6 +8,23 @@
 #   (https://github.com/fabriziocucci/git-bash-for-mac)
 # )
 # Author: Jiacheng Zhao (John)
+
+# Check if git is installed
+if ! type "git" > /dev/null; then
+    # Prompt user to install git
+    echo -e "\033[31;1m\"Git\" is not installed. Please install \"Git\" first before you run this program. \033[m"
+    echo -e "\033[4mHint:\033[m"
+    echo -e "1. Install \"Git\" by running the following command (if you have \033[4mHomebrew\033[m installed):"
+    echo -e "   \033[47mbrew install git\033[m"
+    echo -e "2. Install \"Git\" by running the following command (if you have \033[4mMacPorts\033[m installed):"
+    echo -e "   \033[47msudo port install git\033[m"
+    echo -e "3. Build and install \"Git\" from source code"
+    echo -e "   (you can find the tourial on \033[4mhttps://github.com/git/git/blob/master/INSTALL\033[m)"
+    echo -e "4. Download and install the binary from \033[4mhttps://git-scm.com/downloads\033[m"
+    exit 1
+fi
+
+# Download the support script from GitHub
 curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 
 # Add the support file to .zshrc
