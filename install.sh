a17e8021d4d62d2b277b0faa6f6828dc51a16401
Techzjc <techzjc@vt.edu>
2022-09-30 01:32:27 -0400

    Feat: Shell for git-bash over zsh implementation
    Add the installation shell file for the git-bash over zsh.

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+# Git Bash-like shell settings for zsh
+# Description: 
+# This is a zsh implementation which looks like Git Bash 
+# (
+#    Modified from the GitHub Project:
+#    fabriziocucci/git-bash-for-mac
+#   (https://github.com/fabriziocucci/git-bash-for-mac)
+# )
+# Author: Jiacheng Zhao (John)
+curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+
+# Add the support file to .zshrc
+echo "" >> ~/.zshrc
+echo "source ~/.git-prompt.sh" >> ~/.zshrc
+# Add the 
+echo "PS1=\"%F{green}\`whoami\`@%m%f %F{yellow}%~%f %F{cyan}\"'\$(__git_ps1 \" (%s)\")'\"%f\"\$'\x5cn'\"\$ \"" >> ~/.zshrc
+echo "" >> ~/.zshrc
+
+source ~/.zshrc
