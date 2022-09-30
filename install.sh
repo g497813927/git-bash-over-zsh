35253d6726fadf70f6703f75de1ab192ea26b8f9
Techzjc <techzjc@vt.edu>
2022-09-30 02:08:55 -0400

    Fix: comment part at install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contri
 echo "" >> ~/.zshrc
 echo "# Git Bash-like shell settings for zsh" >> ~/.zshrc
 echo "source ~/.git-prompt.sh" >> ~/.zshrc
-# Add the 
+# Add the customization to .zshrc
 echo "PS1=\"%F{green}\`whoami\`@%m%f %F{yellow}%~%f %F{cyan}\"'\$(__git_ps1 \" (%s)\")'\"%f\"\$'\x5cn'\"\$ \"" >> ~/.zshrc
 echo "" >> ~/.zshrc
 
