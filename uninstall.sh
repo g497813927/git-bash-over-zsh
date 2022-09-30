847e8656ff60301141911599bad4247e45f2a8cb
Dingwen Wang <wangdingwen32@gmail.com>
2022-09-30 01:56:23 -0400

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -18,40 +18,20 @@ fi
 # check zshrc file
 # itraeate through each line
 delete=0
-deleted=0
 while read -r line || [[ -n "$line" ]]; do
     # if the line is Git Bash-like shell settings for zsh
     if [[ $line == "# Git Bash-like shell settings for zsh" ]]; then
         delete=4
-        deleted=1
     fi
     # if delete is 0, write the line to a temp file
     # else, skip the line and decrement delete
     if [[ $delete == 0 ]]; then
         echo $line >> $SHELL_RC.tmp
     else
-        # check if the line is empty, or start with `PS1=`, if not, reject operation and stop
-        if [[ $line == "" || $line == "PS1="* ]]; then
-            delete=$((delete-1))
-        else
-            echo "You might have modified your .zshrc file, exiting..."
-            # remove temp file
-            if [ -f $GIT_PROMPT.tmp ]; then
-                rm $GIT_PROMPT.tmp
-            fi
-            exit 1
-        fi
+        delete=$((delete-1))
     fi
 done < "$SHELL_RC"
 
-# if deleted is 1, replace the original .zshrc file with the temp file
-if [[ $deleted == 1 ]]; then
-    mv $SHELL_RC.tmp $SHELL_RC
-else
-    echo "Git Bash-like shell have never been installed, or you might have modified your .zshrc file."
-    # remove temp file
-    if [ -f $GIT_PROMPT.tmp ]; then
-        rm $GIT_PROMPT.tmp
-    fi
-    exit 1
-fi
+# replace zshrc with temp file
+mv $SHELL_RC $SHELL_RC.bak
+mv $SHELL_RC.tmp $SHELL_RC
